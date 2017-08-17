b7f388970e4cc285b136b1b8bb5075a6a12f0203
Paul Wong <paulwcy@gmail.com>
2017-08-17 14:50:40 +1000

    fix react native 0.47 compilation issues

--- android/src/main/java/com/joshblour/reactnativepermissions/ReactNativePermissionsPackage.java
+++ android/src/main/java/com/joshblour/reactnativepermissions/ReactNativePermissionsPackage.java
@@ -8,17 +8,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class ReactNativePermissionsPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new ReactNativePermissionsModule(reactContext));
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
     }
 
     @Override
